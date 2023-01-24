ed2a36b94ae2bd1c942f38ae53253eab65ac44f9
sumomo1923 <121812671+sumomo1923@users.noreply.github.com>
2023-01-25 01:47:54 +0900

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-MEDIA_URL ="/audio/"
+MEDIA_URL ="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
